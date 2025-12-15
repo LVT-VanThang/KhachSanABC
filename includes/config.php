af453d6e457ef9818e457d183076d954a003817d
LVT-VanThang <DH52201436@student.stu.edu.vn>
2025-12-15 16:09:03 +0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
-// Cấu hình lấy thông tin tự động từ tab Variables của Railway
-define('DB_HOST', getenv('MYSQLHOST'));     // Sẽ lấy giá trị mysql.railway.internal
-define('DB_USER', getenv('MYSQLUSER'));     // Sẽ lấy giá trị root
-define('DB_PASS', getenv('MYSQLPASSWORD')); // Sẽ lấy giá trị EnDGToMs...
-define('DB_NAME', getenv('MYSQLDATABASE')); // Sẽ lấy giá trị railway
-define('DB_PORT', getenv('MYSQLPORT'));     // Sẽ lấy giá trị 3306
+// Lấy thông số tự động từ Railway
+define('DB_HOST', getenv('MYSQLHOST'));
+define('DB_USER', getenv('MYSQLUSER'));
+define('DB_PASS', getenv('MYSQLPASSWORD'));
+define('DB_NAME', getenv('MYSQLDATABASE'));
+define('DB_PORT', getenv('MYSQLPORT'));
 ?>
