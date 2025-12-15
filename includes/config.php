40aed815f6850ee4809f215585aebdff4577aa21
LVT-VanThang <DH52201436@student.stu.edu.vn>
2025-12-15 15:50:46 +0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,6 @@
 <?php
-// THÔNG TIN KẾT NỐI TRÊN RAILWAY
-define('DB_HOST', getenv('mysql.railway.internal')); 
-define('DB_USER', getenv('root'));            
-define('DB_PASS', getenv('EnDGToMsQnizpMrMaDLfbNRVweYhxWCf'));        
-define('DB_NAME', getenv('railway'));
-define('DB_PORT', getenv('3306'));
-?>
+define('DB_HOST', getenv('MYSQLHOST'));
+define('DB_USER', getenv('MYSQLUSER'));
+define('DB_PASS', getenv('MYSQLPASSWORD'));
+define('DB_NAME', getenv('MYSQLDATABASE'));
+define('DB_PORT', getenv('MYSQLPORT'));
