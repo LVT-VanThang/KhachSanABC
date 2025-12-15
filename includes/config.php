e5e81f0dbb8eee571a34ecc894b19787a4e75c43
LVT-VanThang <DH52201436@student.stu.edu.vn>
2025-12-15 15:57:20 +0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,8 @@
 <?php
+// Cấu hình lấy thông tin tự động từ hệ thống Railway
 define('DB_HOST', getenv('MYSQLHOST'));
 define('DB_USER', getenv('MYSQLUSER'));
 define('DB_PASS', getenv('MYSQLPASSWORD'));
 define('DB_NAME', getenv('MYSQLDATABASE'));
-define('DB_PORT', getenv('MYSQLPORT'));
+define('DB_PORT', getenv('MYSQLPORT') ?: 3306); // Dùng cổng Railway cấp, mặc định là 3306
+?>
